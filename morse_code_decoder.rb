584f534a88f7620f7fffae36a02a75b5bad308a3
HENRY-KC <henrykc24@yahoo.com>
2021-12-09 19:14:04 +0100

    Add github linters

--- morse_code_decoder.rb
+++ morse_code_decoder.rb
@@ -1,4 +1,3 @@
-
 def decode_char(str)
   morse = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F',
@@ -11,19 +10,19 @@ def decode_char(str)
 end
 
 def decode_word(str)
-    word_split = str.split(" ")
-    decoded_word = ""
-    word_split.each do |letter| 
-        decoded_word += decode_char(letter)
-    end
-    decoded_word 
+  word_split = str.split(" ")
+  decoded_word = ""
+  word_split.each do |letter|
+    decoded_word += decode_char(letter)
+  end
+  decoded_word
 end
 
 def decode(str)
-    sentence_split = str.split("   ")
-    full_sentence_decoded = ""
-    sentence_split.each do |word|
-        full_sentence_decoded +=  decode_word(word) + " "
-    end
-    full_sentence_decoded.strip
+  sentence_split = str.split("   ")
+  full_sentence_decoded = ""
+  sentence_split.each do |word|
+    full_sentence_decoded += decode_word(word) + " "
+  end
+  full_sentence_decoded.strip
 end
